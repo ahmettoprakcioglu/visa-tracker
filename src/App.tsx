c2310374302ee2c4fecd3626cb8a53cffb838a9c
Ahmet Kaan Toprakçıoğlu <56438853+ahmettoprakcioglu@users.noreply.github.com>
2025-03-11 09:53:31 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <div>
+      <div>App</div>
+      <Button>Visa Tracker</Button>
+    </div>
+  )
+}
+
+export default App
